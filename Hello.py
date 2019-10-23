3cd1c3dbfdae0741d0048ddcea9af72da2336881
ikedatomoki <t.ikeda@vinx.co.jp>
2019-10-23 17:54:59 +0900

    FlaskからHTMLへの値の受け渡し

--- Hello.py
+++ Hello.py
@@ -1,14 +1,21 @@
 #%%
+# coding: utf-8
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    name = "Hello,Flask!!!"
-    # return name
-    return render_template('layout.html', name=name) #HTMLレンダーバージョン
+    return "Hello,Flask!!!"
+
+@app.route("/index") #HTMLレンダーバージョン
+def index():
+    name = "Hello,Flask&HTML!!!"
+    return render_template('index.html', message=name) 
 
 ## おまじない
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run() 
+
+
+#%%
